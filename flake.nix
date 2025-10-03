c94e8f0d1c84bf7a6d2ea683b86fc5c5389809cb
aliensprout <230080509+aliensprout@users.noreply.github.com>
2025-10-03 05:23:41 -0700

    dpi change

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           alacritty
-          qtile
+          python3Packages.qtile
           rofi
           picom
           xwallpaper
